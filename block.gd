966e81e59a3f256a0232f343ad644988186b1441
SugmaLahma <aleksis.koivula@edu.turkuamk.fi>
2024-11-15 20:26:54 +0200

    Coin, sky, colour
    Added texture for coin, made a sky color to reflect stuff. added colour for objects.

---
+++ block.gd
@@ -0,0 +1,19 @@
+extends RigidBody3D
+var player
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	pass # Replace with function body.
+	
+func _on_hurt_box_body_entered(body: Node3D) -> void:
+	pass
