d83b6caa7e83b7bc81268e494688f603f2600128
SugmaLahma <aleksis.koivula@edu.turkuamk.fi>
2024-11-27 14:42:54 +0200

    Character Rotation
    Neede to change Chracter mesh from mesh instance to node 2d because reasons

--- player.gd
+++ player.gd
@@ -5,6 +5,9 @@ const JUMP_VELOCITY = 4.5
 const PUSH_FORCE = 1.0  # Adjust to control the strength of the push
 const MIN_PUSH_FORCE = 0.5  # Minimum force applied when pushing
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")  # Retrieve default gravity setting
+var last_direction = Vector3.FORWARD
+var rotation_speed = 10 #Speed the character rotates
+
 
 func _physics_process(delta: float) -> void:
 	# Add gravity if the player is not on the floor
@@ -19,12 +22,16 @@ func _physics_process(delta: float) -> void:
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction != Vector3.ZERO:
+		last_direction = direction #Checks direction to use for rotation
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		
+	$CharacterMesh.rotation.y = lerp_angle($CharacterMesh.rotation.y, atan2(+last_direction.x, +last_direction.z), delta * rotation_speed) #Rotation for chracter mesh
+	$CollisionShape3D.rotation.y = lerp_angle($CollisionShape3D.rotation.y, atan2(+last_direction.x, +last_direction.z), delta * rotation_speed) #Rotation for chracter hitbox
+	
 	# Move the character and apply the calculated velocity
 	move_and_slide()
 
