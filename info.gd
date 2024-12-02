1d8c29c071b104d30092e581b4ded5bfc0d00b0f
SugmaLahma <aleksis.koivula@edu.turkuamk.fi>
2024-12-02 19:09:02 +0200

    Added sound + skip button
    Added a click sound for icons and a skip button for intro

--- info.gd
+++ info.gd
@@ -3,6 +3,17 @@ extends Node2D
 func _ready():
 	$Fade_transition.hide()  # Hide the fade transition at the start
 
+
+#Skip to game when space is pressed
+#func _physics_process(delta: float) -> void:
+	#if Input.is_action_just_pressed("ui_accept"):
+		#get_tree().change_scene_to_file("res://level_1.tscn")
+	
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene_to_file("res://level_1.tscn")
+		
+	
 # Called when the video finishes
 func _on_VideoStreamPlayer_finished():
 	$Fade_transition.show()  # Show the fade effect
@@ -13,5 +24,4 @@ func _on_fade_timer_timeout():
 	pass
 
 func _on_video_stream_player_finished() -> void:
-	print(1)
 	get_tree().change_scene_to_file("res://level_1.tscn")
--- title_screen.gd
+++ title_screen.gd
@@ -5,15 +5,23 @@ var button_type = null
 # Called when the play button is pressed.
 func _on_play_pressed():
 	button_type = "start"
+	$Click.play()
 	$Fade_transition.show()
 	$Fade_transition/Fade_timer.start()
 	$Fade_transition/AnimationPlayer.play("fade_in")
 
 # Called when the quit button is pressed.
 func _on_quit_pressed():
-	get_tree().quit()
+	$Click.play()
+	$QuitTimer.start()
+
+
 
 # Called when the fade timer times out.
 func _on_fade_timer_timeout() -> void:
 	if button_type == "start":
 		get_tree().change_scene_to_file("res://info.tscn")
+
+
+func _on_quit_timer_timeout() -> void:
+	get_tree().quit()
